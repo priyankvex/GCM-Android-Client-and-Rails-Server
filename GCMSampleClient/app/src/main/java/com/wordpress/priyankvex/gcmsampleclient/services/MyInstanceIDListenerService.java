81a41176c788937bae4f7acd7b6e85372f16bf30
Priyank Verma <priyank.rip@gmail.com>
2015-09-09 23:42:25 +0530

    Add stub InstanceIDListenerService.

---
+++ GCMSampleClient/app/src/main/java/com/wordpress/priyankvex/gcmsampleclient/services/MyInstanceIDListenerService.java
@@ -0,0 +1,18 @@
+package com.wordpress.priyankvex.gcmsampleclient.services;
+
+import com.google.android.gms.iid.InstanceIDListenerService;
+
+/**
+ * Created by Priyank(@priyankvex) on 9/9/15.
+ *
+ * Service is invoked when token(device ID for GCM) is updated.
+ * Could be called when security of the token is compromised.
+ */
+public class MyInstanceIDListenerService extends InstanceIDListenerService{
+
+    @Override
+    public void onTokenRefresh() {
+        super.onTokenRefresh();
+        // Fetch updated token from the server again and send it to application server.
+    }
+}
