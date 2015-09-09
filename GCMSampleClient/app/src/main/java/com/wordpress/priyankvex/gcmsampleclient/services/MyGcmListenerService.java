b4f70807896526e36d086a3d0ba34a3ede26d014
Priyank Verma <priyank.rip@gmail.com>
2015-09-09 23:36:41 +0530

    Add stub GcmListenerService.

---
+++ GCMSampleClient/app/src/main/java/com/wordpress/priyankvex/gcmsampleclient/services/MyGcmListenerService.java
@@ -0,0 +1,24 @@
+package com.wordpress.priyankvex.gcmsampleclient.services;
+
+import android.os.Bundle;
+
+import com.google.android.gms.gcm.GcmListenerService;
+
+/**
+ * Created by Priyank(@priyankvex) on 9/9/15.
+ * Service that is invoked when message is received by the device.
+ */
+public class MyGcmListenerService extends GcmListenerService {
+
+    /**
+     * This method run executed when message is received.
+     * @param from Sender ID of the sender.
+     * @param data message that has been received.
+     */
+    @Override
+    public void onMessageReceived(String from, Bundle data) {
+        super.onMessageReceived(from, data);
+
+
+    }
+}
